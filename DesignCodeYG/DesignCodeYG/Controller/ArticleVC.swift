f46868ae7869ab935f35aa1baade032eee9e534a
Soufiane Salouf <soufiane.salouf@gmail.com>
2018-03-14 22:52:49 +0000

    replacing tableView

--- DesignCodeYG/DesignCodeYG/Controller/ArticleVC.swift
+++ DesignCodeYG/DesignCodeYG/Controller/ArticleVC.swift
@@ -11,7 +11,7 @@ import UIKit
 class ArticleVC: UIViewController {
     
     //Outlets
-    @IBOutlet weak var articleTableView: UITableView!
+    @IBOutlet weak var containerView: UIView!
     @IBOutlet weak var sectionSubTitleLbl: UILabel!
     @IBOutlet weak var sectionTitleLbl: UILabel!
     @IBOutlet weak var sectionItemImage: UIImageView!
@@ -22,10 +22,10 @@ class ArticleVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        articleTableView.delegate = self
-        articleTableView.dataSource = self
-        articleTableView.estimatedRowHeight = 97
-        articleTableView.rowHeight = UITableViewAutomaticDimension
+//        articleTableView.delegate = self
+//        articleTableView.dataSource = self
+//        articleTableView.estimatedRowHeight = 97
+//        articleTableView.rowHeight = UITableViewAutomaticDimension
         setupView()
     }
     
@@ -38,6 +38,13 @@ class ArticleVC: UIViewController {
         sectionTitleLbl.text = section.sectionTitle
         sectionSubTitleLbl.text = section.sectionSubTitle
         sectionItemImage.image = UIImage(named: section.sectionImage)
+        
+//        for index in 0 ..< section.article.count {
+//            let article = section.article[index]
+//                     self.containerView.addSubview(articleView!)
+//
+//        }
+        
     }
     
     @IBAction func closeBtnWasPressed(_ sender: Any) {
@@ -46,17 +53,43 @@ class ArticleVC: UIViewController {
 
 }
 
-extension ArticleVC: UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.section.article.count
+
+class ArticleView: UIView {
+    var title : String!
+    var detail: String!
+    
+    init(frame: CGRect, title: String, detail: String) {
+        super.init(frame: frame)
+        self.title = title
+        self.detail = detail
+        setLayout()
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if let cell = tableView.dequeueReusableCell(withIdentifier: "ArticleCell", for: indexPath) as?
-            ArticleCell {
-            cell.configureCell(article: self.section.article[indexPath.row])
-            return cell
-        }
-        return UITableViewCell()
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setLayout(){
+        let titleLbl = UILabel(frame: CGRect(x: 10, y: 10, width: 300, height: 23))
+        titleLbl.text = self.title
+        addSubview(titleLbl)
+        
+        
     }
 }
+
+//extension ArticleVC: UITableViewDelegate, UITableViewDataSource {
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return self.section.article.count
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        if let cell = tableView.dequeueReusableCell(withIdentifier: "ArticleCell", for: indexPath) as?
+//            ArticleCell {
+//            cell.configureCell(article: self.section.article[indexPath.row])
+//            return cell
+//        }
+//        return UITableViewCell()
+//    }
+//}
+
